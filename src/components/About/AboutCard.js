04444089ea8356a630d596c8766593b6c86f2623
jndmh <92041858+tinho-mp4@users.noreply.github.com>
2023-10-08 23:02:50 +0100

    intital commit

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,16 +9,16 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-          Hello, I'm <span className="danube">Juned </span>
-            from <span className="danube"> Wales 🏴󠁧󠁢󠁷󠁬󠁳󠁿.</span>
-            <br /> I'm a 2<span className="superscript">nd</span> year student pursuing Software Engineering at Swansea University. 
-            <br />
-            My enthusiasm lies in bringing creative ideas to life through software development and constantly staying on the cutting edge of technology trends.
-            <br />
-            <br />
-            Beyond coding, I thrive on 
-          </p>
+        <p style={{ textAlign: "justify" }}>
+          Hello, I'm <span className="danube">Juned</span> from <span className="danube">Wales 🏴󠁧󠁢󠁷󠁬󠁳󠁿</span>.
+          <br />
+        I'm a 2<span className="superscript">nd</span> year student pursuing Software Engineering at Swansea University.
+        <br />
+          My enthusiasm lies in bringing creative ideas to life through software development and constantly staying on the cutting edge of technology trends.
+          <br />
+          <br />
+          Beyond coding, I thrive on...
+        </p>
           <ul>
             <li className="about-activity">
               <SiRiotgames /> Playing Games
